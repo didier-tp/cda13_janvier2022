e0924a3fd8a90befee95d1a00b614afe9dce11a4
didierDefrance <didier@d-defrance.fr>
2022-01-12 15:11:07 +0100

    objet Devise

--- mesTps/s6.js
+++ mesTps/s6.js
@@ -8,6 +8,16 @@ function Devise(code, nom, change){
     }
 }
 
+var d1 = new Devise("DKK" , "Couronne Danemark" , 7.7);
+d1.couleur = "rouge";//possible mais pas conseillé
+
+console.log("d1=" + d1); //[oject Object]
+d1.display();
+console.log("d1 as JSON string = " +  JSON.stringify(d1) );
+console.log("code de d1=" + d1.code); //tout est par défaut public en javascript
+console.log("code de d1=" + d1["code"]); //possible avec objet vu comme tableau associatif 
+
+
 var tabDevises = [];
 tabDevises.push(new Devise("USD","Dollar",1.0));
 tabDevises.push(new Devise("EUR","Euro",0.9));
